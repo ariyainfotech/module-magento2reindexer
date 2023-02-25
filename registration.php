461301c3afd4164a7ba9b548b53fd6e305709b01
Ariya InfoTech <toariyainfotech@gmail.com>
2023-02-25 14:51:35 +0530

    Module-update

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Develop by Ariya InfoTech ( Yuvraj Raulji - www.yuvrajraulji.com ) All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'AriyaInfoTech_BackendReindexer', __DIR__);
+
